e4d2f1ce51aed4b25360801fa8b8ebb481fa1bf8
kkozlovsky <kkozlovsky@yandex.ru>
2024-03-08 17:16:04 +0300

    Initial commit

---
+++ src/main/java/ru/kerporation/datageneratorgrpcmicroservice/DataGeneratorGrpcMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.kerporation.datageneratorgrpcmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataGeneratorGrpcMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataGeneratorGrpcMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kerporation/datageneratorgrpcmicroservice/DataGeneratorGrpcMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kerporation.datageneratorgrpcmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataGeneratorGrpcMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
